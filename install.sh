09850a59f28a3c882f69319bdcc50dc2e2f2f579
Cesar Ramirez <me@cramsan.com>
2023-04-08 12:57:37 -0700

    Updating installation steps (#21)

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ARTIFACT_FILE="derpserver.jar"
+ARTIFACT_FOLDER="server/build/libs"
+
+INSTALLATION_FOLDER="/opt/derpserver/"
+
+PROPERTIES_FILE="application.properties"
+SYSTEMD_FILE="derpserver.service"
+
+echo "Building Server Executable"
+sleep 2
+./gradlew server:bootJar
+
+echo "Creating $INSTALLATION_FOLDER"
+mkdir -p $INSTALLATION_FOLDER
+
+touch $INSTALLATION_FOLDER/$PROPERTIES_FILE
+
+systemctl stop $SYSTEMD_FILE
+
+cp $ARTIFACT_FOLDER/$ARTIFACT_FILE $INSTALLATION_FOLDER/$ARTIFACT_FILE
+
+sudo cp $SYSTEMD_FILE /etc/systemd/system/$SYSTEMD_FILE
+
+systemctl daemon-reload
+
+systemctl restart $SYSTEMD_FILE
